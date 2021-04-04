81ab65f717301ec3f9707a313e81982c237afa77
EuSet <setskoeugene@inbox.ru>
2021-04-04 13:29:08 +0300

    added localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from "./Counter";
 
 
 function App() {
   const [value, setValue] = useState<number>(0)
+  useEffect(() => {
+    const valueAsString = localStorage.getItem('counterValue')
+    if(valueAsString){
+      const newValue = JSON.parse(valueAsString)
+      setValue(newValue)
+    }
+  },[])
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    },[value])
   const counterPlus = () => {
     if(value < 5){
       setValue(value+1)
@@ -18,7 +28,6 @@ function App() {
       setValue(0)
     }
   }
-
   return (
       <div className="App">
         <Counter
